2ba105db1bb1890f105d11bcb21c01121ae4c936
Apple <ilafayezalm@gmail.com>
2025-12-12 17:40:54 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: { 'saudi-green': '#006C35' },
+      fontFamily: { sans: ['Tajawal', 'sans-serif'] }
+    },
+  },
+  plugins: [],
+}
